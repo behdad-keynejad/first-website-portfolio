f9842194a92f72af3a9814707b168142cdfe8db8
Carol Yao <c.y-@hotmail.com>
2016-08-01 22:41:16 -0400

    changed to new theme with bootstrap and added parallax scroll

---
+++ README.md
@@ -0,0 +1,6 @@
+Hi I'm Carol .
+
+Feel free to check out my website :) 
+
+https://carol-yao.github.io/
+
--- scripts.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $('.code-project-image').on('hover', function() {
-    $(this).css('opacity', 0.2);
-  })
-})
